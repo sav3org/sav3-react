c8defca92ce3b3fc0c952d7329a21425100dab78
spacex07 <76837937+spacex07@users.noreply.github.com>
2021-01-02 01:58:43 +0300

    Turkısh Leanguage
    Added Turkısh leanguage

--- src/translations/translations/en-us.js
+++ src/translations/translations/en-us.js
@@ -2,31 +2,31 @@ import createTranslation from '../create-translation'
 
 const translation = createTranslation({
   // profile
-  Follow: () => 'Follow',
-  Unfollow: () => 'Unfollow',
-  'Edit profile': () => 'Edit profile',
-  'Display name': () => 'Display name',
-  Description: () => 'Description',
-  'Thumbnail URL': () => 'Thumbnail URL',
-  'Banner URL': () => 'Banner URL',
+  Follow: () => 'Takip et',
+  Unfollow: () => 'Takibi bırak',
+  'Edit profile': () => 'Profili düzenle',
+  'Display name': () => 'Görüntülecek isim',
+  Description: () => 'Açıklama',
+  'Thumbnail URL': () => 'Profil resmi URL’si',
+  'Banner URL': () => 'Kapak resmi URL’si',
 
   // menu
-  Home: () => 'Home',
-  Profile: () => 'Profile',
-  Search: () => 'Search',
-  'Search user ID': () => 'Search user ID',
-  Peers: () => 'Peers',
-  'Connected peers posts': () => 'Connected peers posts',
-  Stats: () => 'Stats',
-  'Connected peers stats': () => 'Connected peers stats',
-  'Connecting to peers': () => 'Connecting to peers',
-  Following: () => 'Following',
-  'Not following anyone': () => 'Not following anyone',
-  Export: () => 'Export',
-  Import: () => 'Import',
+  Home: () => 'Anasayfa',
+  Profile: () => 'Profil',
+  Search: () => 'Ara',
+  'Search user ID': () => 'Kullanıcı adı Ara',
+  Peers: () => 'Eşler',
+  'Connected peers posts': () => 'Eşlerin gönderilerine bağlandı',
+  Stats: () => 'İstatistikler',
+  'Connected peers stats': () => 'Eşlerin istatistiklerine bağlandı',
+  'Connecting to peers': () => 'Eşlere bağlanıyor',
+  Following: () => 'Takip ediliyor',
+  'Not following anyone': () => 'Hiç kimseyi takip etmiyorsunuz',
+  Export: () => 'Dışarı aktar',
+  Import: () => 'İçeri aktar',
 
   // post
-  'Uncensorable content': () => 'Uncensorable content'
+  'Uncensorable content': () => 'Sansürsüz içerik'
 })
 
 export default translation
